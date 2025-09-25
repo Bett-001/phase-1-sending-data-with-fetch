f194c5e72ef1e1a2a91c01d618a0427af377caf3
Tonnie-debug-svg <tonniekbett@gmail.com>
2025-09-26 00:48:38 +0300

    Completed sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+    .then((response) => response.json())
+    .then((object) => {
+      // append new id to the DOM
+      const body = document.querySelector("body");
+      const p = document.createElement("p");
+      p.textContent = `New user created with ID: ${object.id}`;
+      body.appendChild(p);
+      return object;
+    })
+    .catch((error) => {
+      // append error message to the DOM
+      const body = document.querySelector("body");
+      const p = document.createElement("p");
+      p.textContent = error.message;
+      body.appendChild(p);
+    });
+}
+
